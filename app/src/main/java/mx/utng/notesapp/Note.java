655a3a21194acd168b3233d55d8a335cb10e9c66
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-07-11 23:38:55 -0500

    first commit

---
+++ app/src/main/java/mx/utng/notesapp/Note.java
@@ -0,0 +1,19 @@
+package mx.utng.notesapp;
+
+public class Note {
+    private String title = "";
+    private String id = "";
+
+    public Note(String id, String title) {
+        this.title = title;
+        this.id = id;
+    }
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+}
