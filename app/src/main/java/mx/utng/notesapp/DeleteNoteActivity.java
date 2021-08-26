81efb572c0a2b5dbf7229a9bfc0f4193ba971434
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-08-26 15:59:47 -0500

    arreglar borrado forzado

--- app/src/main/java/mx/utng/notesapp/DeleteNoteActivity.java
+++ app/src/main/java/mx/utng/notesapp/DeleteNoteActivity.java
@@ -1,10 +1,14 @@
 package mx.utng.notesapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import mx.utng.notesapp.databinding.ActivityDeleteNoteBinding;
@@ -13,6 +17,7 @@ public class DeleteNoteActivity extends Activity {
 
     private ActivityDeleteNoteBinding binding;
     Button btnCancelar;
+    private int flag = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,24 @@ public class DeleteNoteActivity extends Activity {
             @Override
             public void onClick(View view) {
                 finish();
+                flag = 0;
             }
         });
+        CountDownTimer cuenta = new CountDownTimer(3000,1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                if(flag == 1) {
+                    Helper.displayConfirmation("Eliminada", DeleteNoteActivity.this);
+                    String id = getIntent().getStringExtra("id");
+                    Helper.eliminarNota(id, DeleteNoteActivity.this);
+                    finish();
+                }
+            }
+        }.start();
     }
 }
